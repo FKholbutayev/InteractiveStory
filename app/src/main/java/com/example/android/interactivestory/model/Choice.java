20aa57de360b5048cf43802465edb57e77d48b39
extrahappyperson <ht.uzpharma@gmail.com>
2018-01-31 23:11:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package com.example.android.interactivestory.model;
+
+/**
+ * Created by Administrator on 2018-01-28.
+ */
+
+public class Choice {
+    private int textId;
+    private int nextPage;
+
+    public Choice(int textId, int nextPage) {
+        this.textId = textId;
+        this.nextPage = nextPage;
+    }
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
+
+    public int getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
+    }
+}
---
+++ app/src/main/java/com/example/android/interactivestory/ui/StoryActivity.java
@@ -0,0 +1,114 @@
+package com.example.android.interactivestory.ui;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.android.interactivestory.R;
+import com.example.android.interactivestory.model.Page;
+import com.example.android.interactivestory.model.Story;
+
+import java.util.Stack;
+
+public class StoryActivity extends AppCompatActivity {
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
+    private String name;
+    private Stack<Integer> pageStack = new Stack<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        storyImageView = findViewById(R.id.storyimageView);
+        storyTextView = findViewById(R.id.StoryTextView);
+        choice1Button = findViewById(R.id.choice1Button);
+        choice2Button = findViewById(R.id.choice2Button);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra(getString(R.string.key_name));
+        if(name == null || name.isEmpty()) {
+            name = "Friend";
+        }
+
+        Log.d(TAG, name);
+        story = new Story();
+        loadPage(0);
+
+
+
+    }
+
+    private void loadPage(int pageNumber) {
+        pageStack.push(pageNumber);
+        final Page page = story.getPage(pageNumber);
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+        storyImageView.setImageDrawable(image);
+
+        String pageText = getString(page.getTextId());
+        pageText = String.format(pageText, name);
+        storyTextView.setText(pageText);
+
+        if(page.isFinalPage()) {
+            choice1Button.setVisibility(View.INVISIBLE);
+            choice2Button.setText(R.string.play_again_text);
+            choice2Button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    loadPage(0);
+                }
+            });
+        } else {
+            loadButtons(page);
+        }
+    }
+
+    private void loadButtons(final Page page) {
+        choice1Button.setVisibility(View.VISIBLE);
+        choice1Button.setText(page.getChoice1().getTextId());
+        choice1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice1().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+        choice2Button.setVisibility(View.VISIBLE);
+        choice2Button.setText(page.getChoice2().getTextId());
+        choice2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        pageStack.pop();
+        if(pageStack.isEmpty()) {
+            super.onBackPressed();
+
+        } else {
+            loadPage(pageStack.pop());
+        }
+
+    }
+}
+
+
